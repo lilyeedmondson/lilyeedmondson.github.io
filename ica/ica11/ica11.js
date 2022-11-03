ea0acbd5391e15fe4a09bffd541ec4196a41ec98
lilyeedmondson <89483445+lilyeedmondson@users.noreply.github.com>
2022-11-02 22:32:11 -0600

    ica11 finish

--- ica/ica11/ica11.js
+++ ica/ica11/ica11.js
@@ -1,27 +1,33 @@
+window.addEventListener('onload', getQuote);
+
 const quoteButton = document.querySelector('.new-quote');
 quoteButton.addEventListener('click', getQuote);
 
-let trump = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
 
 async function getQuote() {
-    //console.log('test click worked');
-    let text = await fetch(trump);
+    console.log('test click worked');
+    let text = await fetch(endpoint);
     let response = await text.text();
 
     let json_response = JSON.parse(response);
+    console.log (json_response);
     console.log(json_response['message']);
     //.then(text => text.text());
+    // .then(y=> myDisplay(y));
     //console.log(text['message']);
+
     displayQuote(json_response['message']);
-    }
+}
 
-function displayQuote(x) {
-    const quoteBox = document.querySelector('#js-quote-text');
-    //console.log('display quote working');
-    //let textMessage = document.createTextNode(x);
+function displayQuote(x){
+    //const quoteBox = document.querySelector('#js-quote-text');
+    // //console.log('display quote working');
+    
 
-    // quoteBox.appendChild(textMesssage);
+   //const textMessage = document.createTextNode(x);
+   //quoteBox.appendChild(textMessage);
     document.getElementById('js-quote-text').textContent = x;
 }
 
-getQuote(); 
+getQuote();
