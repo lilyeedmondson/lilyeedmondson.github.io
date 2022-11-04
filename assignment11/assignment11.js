70dfab85349a0b2584f0270b6e91e4fbb4dbcaf7
lilyeedmondson <89483445+lilyeedmondson@users.noreply.github.com>
2022-11-04 16:16:49 -0600

    assignment11start

---
+++ assignment11/assignment11.js
@@ -0,0 +1,33 @@
+window.addEventListener('onload', getQuote);
+
+const quoteButton = document.querySelector('.new-quote');
+quoteButton.addEventListener('click', getQuote);
+
+const endpoint = 'https://api.funtranslations.com/translate/yoda.json';
+
+async function getQuote() {
+    console.log('test click worked');
+    let text = await fetch(endpoint);
+    let response = await text.text();
+
+    let json_response = JSON.parse(response);
+    console.log (json_response);
+    console.log(json_response['message']);
+    //.then(text => text.text());
+    // .then(y=> myDisplay(y));
+    //console.log(text['message']);
+
+    displayQuote(json_response['message']);
+}
+
+function displayQuote(x){
+    //const quoteBox = document.querySelector('#js-quote-text');
+    // //console.log('display quote working');
+    
+
+   //const textMessage = document.createTextNode(x);
+   //quoteBox.appendChild(textMessage);
+    document.getElementById('js-quote-text').textContent = x;
+}
+
+getQuote();
